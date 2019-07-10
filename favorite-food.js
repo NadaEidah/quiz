4403bc3bb1ae34cb4b715e2cd7a24552d85c35d1
NadaEidah <44302867+NadaEidah@users.noreply.github.com>
2019-07-10 12:32:57 +0300

    Add files via upload

---
+++ favorite-food.js
@@ -0,0 +1,7 @@
+/*
+ * Programming Quiz: Favorite Food 
+ * Create a string with the name of your favorite food. The first letter of the string should be capitalized.
+ */
+
+var food="Spaghetti"
+console.log(food);
